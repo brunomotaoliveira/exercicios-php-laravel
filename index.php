a92c5ad86915c1d8d3572869d9a5fb91e48b6bb7
brunomotadev <brunomotadev@gmail.com>
2022-01-03 20:13:08 -0300

    adicionado arquivos bases php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Indice dos Exercicios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>1. Básico</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=ola">Olá PHP</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=html">Integração HTML</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=css">Integração CSS</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=desafio">Desafio PHP</a>
+                        </li>
+                    </ul>
+                </div>
