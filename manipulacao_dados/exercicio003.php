a6044aada07b0286333fd362120461c8db4f00ee
brunomotadev <brunomotadev@gmail.com>
2022-01-03 20:23:19 -0300

    adicionado resolucao do exercicio 3

--- manipulacao_dados/exercicio003.php
+++ manipulacao_dados/exercicio003.php
@@ -56,4 +56,28 @@
 
         // C) mostre o total de cada cliente
         // ["Gabriel" => 300, "Bruno" => 300] -->
+<?php
+        //Exercicio 3
+$valoresBruno = [];
+$valoresGabriel = [];
 
+foreach($pedidos as $pedido) {
+    
+    if($pedido["id_user"] == 1) {
+        
+        array_push($valoresGabriel, ["valor"=>$pedido["valor"], "id_user"=>$pedido["id_user"], "name"=>$pedido["name"]]);
+        print_r(json_encode($pedido));
+    }
+    if($pedido["id_user"] == 2) {
+        array_push($valoresBruno, ["valor"=>$pedido["valor"], "id_user"=>$pedido["id_user"], "name"=>$pedido["name"]]);
+        print_r(json_encode($pedido));
+    }
+    echo '<br>';
+
+}
+
+print_r(["Gabriel"=>array_sum(array_column($valoresGabriel, 'valor'))]);
+echo '<br>';
+print_r(["Bruno"=>array_sum(array_column($valoresBruno, 'valor'))]);
+
+?>
